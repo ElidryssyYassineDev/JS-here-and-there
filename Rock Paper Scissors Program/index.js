a2aedc1aaa4717990aa2c754094a683f5a0d1e95
ELIDRYSSYyassine <kings4breakfast69@gmail.com>
2026-06-15 23:54:38 +0100

    Add JavaScript functionality for Rock Paper Scissor game

---
+++ Rock Paper Scissors Program/index.js
@@ -0,0 +1,7 @@
+let resultEl = document.querySelector(".result");
+let userSCore = 0;
+let ComputerScore = 0;
+resultEl.innerHTML = `<div class="result">
+        your score: ${userSCore}
+        Computer's score:${ComputerScore}
+    </div>`;

5169b6bcf66a79b7c93567f0d7f4dbf60338967b
ELIDRYSSYyassine <kings4breakfast69@gmail.com>
2026-06-16 23:54:40 +0100

    Add JavaScript functionality for Rock Paper Scissor game

--- Rock Paper Scissors Program/index.js
+++ Rock Paper Scissors Program/index.js
@@ -1,7 +1,37 @@
-let resultEl = document.querySelector(".result");
-let userSCore = 0;
+let resultEl = document.querySelector("#result");
+let scoresEl = document.querySelector("#scores");
+let userScore = 0;
 let ComputerScore = 0;
-resultEl.innerHTML = `<div class="result">
-        your score: ${userSCore}
-        Computer's score:${ComputerScore}
-    </div>`;
+
+let buttons = document.querySelectorAll("button");
+
+buttons.forEach(button => {
+    button.addEventListener("click", ()=>{
+        let userChoice = button.name;
+        let computerChoice = buttons[Math.floor(Math.random() * buttons.length)].name;
+
+        switch (userChoice,computerChoice){
+            case ("rock","scissors")|| ("paper","rock") || ("scissors","paper"):
+                userScore++;
+                resultEl.textContent="you won !";
+                scoresEl.innerHTML = `
+                                        <div id="scores">
+                                            your score: <span id="user-score" style="color: green;">${userScore} </span>
+                                            Computer's score: <span id="computer-score" style="color: red;">${ComputerScore} </span>
+                                        </div>`;
+                break;
+            case ("rock","paper") || ("paper","scissors") || ("scissors","rock"):
+                ComputerScore++;
+                resultEl.textContent="you lost!";
+                scoresEl.innerHTML = `
+                                        <div id="scores">
+                                            your score: <span id="user-score" style="color: green;">${userScore} </span>
+                                            Computer's score: <span id="computer-score" style="color: red;">${ComputerScore} </span>
+                                        </div>`;
+                break;
+            default:
+                resultEl.textContent="tie game!";
+        }
+
+    })
+})
